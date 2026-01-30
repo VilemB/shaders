d69cffb4bbdcc65ddd867904ba490f9432c93ab1
VilemB <barnetv7@gmail.com>
2026-01-30 12:38:11 +0100

    modelMatrix & viewMatrix

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,8 +1,13 @@
 uniform mat4 projectionMatrix;
 uniform mat4 modelViewMatrix;
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
 
 attribute vec3 position;
 
 void main() {
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    // modelMatrix -> position, rotation, scale of object
+    // viewMatrix -> position, rotation, scale of camera
+    // projectionMatrix -> projects our object to screen (aspect ratio & the perspective)
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
