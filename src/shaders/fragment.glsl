8b81ea57a9ae1fbac417ba48f4be5c692bacc5dd
VilemB <barnetv7@gmail.com>
2026-01-31 18:01:20 +0100

    box

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -4,6 +4,21 @@ varying vec3 vPosition;
 varying vec3 vNormal;
 varying vec2 vUv;
 
+float drawCircle(vec2 position, vec2 center, float radius) {
+    return step(radius, distance(position, center));
+}
+
+float sdBox( in vec2 p, in vec2 b) {
+    vec2 d = abs(p) - b;
+    return length(max(d, 0.0)) + min(max(d.x, d.y), 0.0);
+}
+
 void main() {
-    gl_FragColor = vec4(vec3(step(0.99, 1.0 - abs(vUv.y - 0.5))), 1.0);
+
+    // line
+    // vec3(step(0.99, 1.0 - abs(vUv.y - 0.5)))
+
+    //
+
+    gl_FragColor = vec4(vec3(step(0.9, 1.0 - sdBox(vUv - .5, vec2(.15)))), 1.0);
 }
